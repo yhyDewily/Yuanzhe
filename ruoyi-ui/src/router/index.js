21aae45b15b478cffc6176a92e5534ece1a1c73b
yhyDewily <1016886553@qq.com>
2022-01-16 11:55:02 +0800

    根据日期查找功能bug修复

--- ruoyi-ui/src/router/index.js
+++ ruoyi-ui/src/router/index.js
@@ -70,7 +70,7 @@ export const constantRoutes = [
         path: 'index',
         component: () => import('@/views/index'),
         name: 'Index',
-        meta: { title: '首页', icon: 'dashboard', affix: true }
+        meta: { title: '证书状态查询', icon: 'dashboard', affix: true }
       }
     ]
   },
