97623f05991d57aec6b9d203bb0a90e098df42e4
lilesheng <1141684696@qq.com>
2022-08-19 15:20:37 +0800

    添加密钥生成，修复添加mybatis-plus框架产生的bug

--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SecretKeyServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SecretKeyServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ruoyi.system.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.ruoyi.system.domain.SecretKey;
 import com.ruoyi.system.mapper.SecretKeyMapper;
 import com.ruoyi.system.service.SecretKeyService;
@@ -11,6 +12,7 @@ import org.bouncycastle.crypto.params.ECPublicKeyParameters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 /**
@@ -30,6 +32,7 @@ public class SecretKeyServiceImpl extends ServiceImpl<SecretKeyMapper, SecretKey
     public Map<String,String> applyKeyPair(String type, String keyName) {
         // 先根据SM2算法生成密钥
         SM2KeyPair keyPair = SM2Util.generateorSM2KeyPair();
+        LocalDateTime creatTime = LocalDateTime.now();
 
         // 拿到公私钥参数
         ECPublicKeyParameters publicKeyParam = keyPair.getPublicKeyParam();
@@ -41,7 +44,19 @@ public class SecretKeyServiceImpl extends ServiceImpl<SecretKeyMapper, SecretKey
         Map<String,String> keyMap = new HashMap<>();
         keyMap.put("publicKey",publicKey);
         keyMap.put("privateKey",privateKey);
+        // 为密钥表插入该密钥信息
+        SecretKey secretKey = new SecretKey();
+        secretKey.setKeyName(keyName);
+        secretKey.setKeySymmetry(false);
+        secretKey.setPublicKey(publicKey);
+        secretKey.setPrivateKey(privateKey);
+        secretKey.setCreateTime(creatTime);
+        secretKey.setSerialNumber("123456789");
+        secretKey.setKeyType("SM2");
+        this.save(secretKey);
         return keyMap;
+
+
         // 获取到公钥
 //        String publicKey = encoder64.encodeToString(publicKeyParam.getQ().getAffineXCoord().getEncoded())
 //                + encoder64.encodeToString(publicKeyParam.getQ().getAffineYCoord().getEncoded());
